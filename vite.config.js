506db2c4e77fdd0d672377d9bd8bb691380926b5
josuanhub <urusgovx@gmail.com>
2026-07-01 19:55:59 -0400

    feat: add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  server: {
+    port: 5173,
+    host: true,
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          icons:  ['lucide-react']
+        }
+      }
+    }
+  }
+})
